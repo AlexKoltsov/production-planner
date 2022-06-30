9993057a5f256545d7a83ff91e93be595a2aad41
Alexander Koltsov <a.koltsov@dats.team>
2022-06-30 19:14:22 +0300

    Initial

---
+++ src/test/java/com/kotlsov/production/planner/ProductionPlannerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kotlsov.production.planner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductionPlannerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
